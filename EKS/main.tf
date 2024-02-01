8637941408c1d9849e6717c6d9a28ca7ac91c0e1
sojstyles <olasoji@olasojiopeyemi.com>
2024-01-31 19:52:34 -0500

    On new branch

--- EKS/main.tf
+++ EKS/main.tf
@@ -9,9 +9,10 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_dns_hostnames = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  map_public_ip_on_launch = true
+  enable_dns_hostnames    = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
 
   tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
@@ -38,7 +39,7 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
 
   eks_managed_node_groups = {
     nodes = {
